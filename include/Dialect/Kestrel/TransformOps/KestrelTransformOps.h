8fb15e2c9207a90f7fc912f49894a56eb17fabd7
Qi Zhuang <qzhuang@amperecomputing.com>
2025-04-29 17:59:02 -0700

    boiler plate and cmakes for optimizer

---
+++ include/Dialect/Kestrel/TransformOps/KestrelTransformOps.h
@@ -0,0 +1,12 @@
+//===- UniqueFuncNameExt.td - Transform dialect extension -----------------===//
+// This file defines a dialect extension for the Transform dialect
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Bytecode/BytecodeOpInterface.h"
+#include "mlir/Dialect/Transform/IR/TransformDialect.h"
+#include "mlir/Dialect/Transform/Interfaces/TransformInterfaces.h"
+
+#define GET_OP_CLASSES
+#include "KestrelTransformOps.h.inc"
+
+void registerKestrelTransformOps(::mlir::DialectRegistry &registry);
