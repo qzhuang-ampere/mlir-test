2c2ae8758df6de374099ae9c54d82d0d9bd573eb
Qi Zhuang <qzhuang@amperecomputing.com>
2025-05-03 16:42:10 -0700

    add essiential kestrel dialect

---
+++ include/Dialect/Kestrel/TransformOps/KestrelOps.h
@@ -0,0 +1,16 @@
+//===- KestrelOps.h - Kestrel dialect ops ---------------------------------===//
+// This is the operation definition header file for the Kestrel dialect.
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_DIALECT_KESTREL_IR_KESTRELOPS_H
+#define MLIR_DIALECT_KESTREL_IR_KESTRELOPS_H
+
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinTypes.h"
+
+#include "KestrelDialect.h.inc"
+
+#define GET_OP_CLASSES
+#include "KestrelOps.h.inc"
+
+#endif // MLIR_DIALECT_KESTREL_IR_KESTRELOPS_H
