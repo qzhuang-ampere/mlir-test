4f533940b614cecafb5b33697881fe700d8c8b5d
Qi Zhuang <qzhuang@amperecomputing.com>
2025-05-19 18:17:38 -0700

    buffierize aice matmul, load, store, function boundary

--- lib/Dialect/Kestrel/TransformOps/BufferizableOpInterfaceImpl.cpp
+++ lib/Dialect/Kestrel/TransformOps/BufferizableOpInterfaceImpl.cpp
@@ -7,6 +7,7 @@
 #include "include/Dialect/Kestrel/TransformOps/BufferizableOpInterfaceImpl.h"
 #include "include/Dialect/Kestrel/TransformOps/KestrelOps.h"
 #include "mlir/Dialect/Bufferization/IR/BufferizableOpInterface.h"
+#include "llvm/Support/Debug.h"
 
 using namespace mlir;
 using namespace mlir::bufferization;
@@ -19,21 +20,43 @@ struct DMALoadOpInterface
                                                     kestrel::DMALoadOp> {
   bool bufferizesToMemoryRead(Operation *op, OpOperand &opOperand,
                               const AnalysisState &state) const {
-    return true;
+    return false;
   }
 
   bool bufferizesToMemoryWrite(Operation *op, OpOperand &opOperand,
                                const AnalysisState &state) const {
-    return true;
+    return false;
   }
 
   AliasingValueList getAliasingValues(Operation *op, OpOperand &opOperand,
                                       const AnalysisState &state) const {
-    return {{op->getOpResult(0), BufferRelation::Unknown}};
+    return {};
   }
 
   LogicalResult bufferize(Operation *op, RewriterBase &rewriter,
                           const BufferizationOptions &options) const {
+    auto dmaLoadOp = cast<kestrel::DMALoadOp>(op);
+    FailureOr<Value> memref =
+        getBuffer(rewriter, dmaLoadOp.getSource(), options);
+    if (failed(memref))
+      return failure();
+
+    auto resultMemrefType =
+        bufferization::getBufferType(dmaLoadOp.getResult(), options);
+    if (failed(resultMemrefType))
+      return failure();
+
+    auto newOp = rewriter.create<kestrel::DMALoadOp>(
+        dmaLoadOp->getLoc(), llvm::cast<MemRefType>(*resultMemrefType),
+        *memref,
+        dmaLoadOp.getOffsets(),
+        dmaLoadOp.getStrides(),
+        dmaLoadOp.getSizes(),
+        dmaLoadOp.getStaticOffsets(),
+        dmaLoadOp.getStaticStrides(),
+        dmaLoadOp.getStaticSizes());
+
+    replaceOpWithBufferizedValues(rewriter, op, newOp.getResult());
     return success();
   }
 };
@@ -43,31 +66,107 @@ struct DMAStoreOpInterface
                                                     kestrel::DMAStoreOp> {
   bool bufferizesToMemoryRead(Operation *op, OpOperand &opOperand,
                               const AnalysisState &state) const {
-    return true;
+    return false;
   }
 
   bool bufferizesToMemoryWrite(Operation *op, OpOperand &opOperand,
                                const AnalysisState &state) const {
-    return true;
+    return false;
   }
 
   AliasingValueList getAliasingValues(Operation *op, OpOperand &opOperand,
                                       const AnalysisState &state) const {
-    return {{op->getOpResult(0), BufferRelation::Unknown}};
+    return {};
   }
 
   LogicalResult bufferize(Operation *op, RewriterBase &rewriter,
                           const BufferizationOptions &options) const {
+    auto dmaStoreOp = cast<kestrel::DMAStoreOp>(op);
+    FailureOr<Value> srcMemref =
+        getBuffer(rewriter, dmaStoreOp.getSource(), options);
+    if (failed(srcMemref))
+      return failure();
+
+    FailureOr<Value> dstMemref =
+        getBuffer(rewriter, dmaStoreOp.getDest(), options);
+    if (failed(dstMemref))
+      return failure();
+
+    auto resultMemrefType =
+        bufferization::getBufferType(dmaStoreOp.getResult(), options);
+    if (failed(resultMemrefType))
+      return failure();
+
+    auto newOp = rewriter.create<kestrel::DMAStoreOp>(
+        dmaStoreOp->getLoc(), llvm::cast<MemRefType>(*resultMemrefType),
+        *srcMemref,
+        *dstMemref,
+        dmaStoreOp.getOffsets(),
+        dmaStoreOp.getStrides(),
+        dmaStoreOp.getSizes(),
+        dmaStoreOp.getStaticOffsets(),
+        dmaStoreOp.getStaticStrides(),
+        dmaStoreOp.getStaticSizes());
+
+    replaceOpWithBufferizedValues(rewriter, op, newOp.getResult());
     return success();
   }
 };
 
-}
-}
+struct AiceMatMulOpInterface
+    : public BufferizableOpInterface::ExternalModel<AiceMatMulOpInterface,
+                                                    kestrel::AiceMatMulOp> {
+  bool bufferizesToMemoryRead(Operation *op, OpOperand &opOperand,
+                              const AnalysisState &state) const {
+    return false;
+  }
+
+  bool bufferizesToMemoryWrite(Operation *op, OpOperand &opOperand,
+                               const AnalysisState &state) const {
+    return false;
+  }
+
+  AliasingValueList getAliasingValues(Operation *op, OpOperand &opOperand,
+                                      const AnalysisState &state) const {
+    return {};
+  }
+
+  LogicalResult bufferize(Operation *op, RewriterBase &rewriter,
+                          const BufferizationOptions &options) const {
+    llvm::dbgs() << "Bufferizing AiceMatMulOp\n";
+    auto aiceMatMulOp = cast<kestrel::AiceMatMulOp>(op);
+    FailureOr<Value> lhsMemref =
+        getBuffer(rewriter, aiceMatMulOp.getLhs(), options);
+    if (failed(lhsMemref))
+      return failure();
+
+    FailureOr<Value> rhsMemref =
+        getBuffer(rewriter, aiceMatMulOp.getRhs(), options);
+    if (failed(rhsMemref))
+      return failure();
+
+    auto resultMemrefType =
+        bufferization::getBufferType(aiceMatMulOp.getResult(), options);
+    if (failed(resultMemrefType))
+      return failure();
+
+    // Create a new AiceMatMulOp with the bufferized operands.
+    auto newOp = rewriter.create<kestrel::AiceMatMulOp>(
+        op->getLoc(), llvm::cast<MemRefType>(*resultMemrefType), *lhsMemref,
+        *rhsMemref);
+
+    replaceOpWithBufferizedValues(rewriter, op, newOp.getResult());
+    return success();
+  }
+};
+
+} // namespace kestrel
+} // namespace mlir
 
 void mlir::kestrel::registerBufferizableOpInterfaceExternalModels(DialectRegistry &registry) {
   registry.addExtension(+[](MLIRContext *ctx, kestrel::KestrelDialect *dialect){
     DMALoadOp::attachInterface<DMALoadOpInterface>(*ctx);
     DMAStoreOp::attachInterface<DMAStoreOpInterface>(*ctx);
+    AiceMatMulOp::attachInterface<AiceMatMulOpInterface>(*ctx);
   });
 }
