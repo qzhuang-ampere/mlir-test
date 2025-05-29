c79764ecf8db839de7fbd69f7e0da18c6c5574b4
Qi Zhuang <qzhuang@amperecomputing.com>
2025-05-28 22:34:25 -0700

    implement the scf.forall merge

--- lib/Dialect/Kestrel/TransformOps/KestrelTransformOps.cpp
+++ lib/Dialect/Kestrel/TransformOps/KestrelTransformOps.cpp
@@ -6,6 +6,8 @@
 #include "KestrelTransformOps.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/SCF/Utils/Utils.h"
 #include "mlir/Dialect/Transform/IR/TransformDialect.h"
 #include "mlir/Dialect/Transform/IR/TransformTypes.h"
@@ -131,19 +133,240 @@ static scf::ExecuteRegionOp wrapInExecuteRegion(RewriterBase &b,
   return DiagnosedSilenceableFailure::success();
 }
 
+mlir::Value getSingleAttributeValueFromOpFoldResult(std::optional<mlir::OpFoldResult> &ofr) {
+  llvm_unreachable("Not implemented");
+  return nullptr;
+}
+
 scf::ForallOp mergeInnerScfForAll(
-    ::mlir::RewriterBase &rewriter, scf::ForallOp forallOp) {
+    ::mlir::RewriterBase &rewriter, scf::ForallOp forallOp, scf::ForallOp innerForallOp) {
   if (forallOp.getNumOperands() != 1)
     return nullptr;
 
-  auto newForallOp = rewriter.create<scf::ForallOp>(
-    forallOp.getLoc(), forallOp.getMixedLowerBound(),
-    forallOp.getMixedUpperBound(), forallOp.getMixedStep(), forallOp.getOutputs(),
-    forallOp.getMapping(),
-    /*bodyBuilderFn =*/[](OpBuilder &, Location, ValueRange) {});
+  // Get the loop variable from innerForallOp
+  auto lowerbound = innerForallOp.getSingleLowerBound();
+  auto upperbound = innerForallOp.getSingleUpperBound();
 
-  rewriter.inlineRegionBefore(forallOp.getRegion(), newForallOp.getRegion(),
-                              newForallOp.getRegion().end());
+  auto attrLb = cast<IntegerAttr>(cast<Attribute>(*lowerbound));
+  if (!attrLb) {
+    llvm::dbgs() << innerForallOp << "expected lower bound to be an IntegerAttr\n";
+    return nullptr;
+  }
+  else {
+    // If the lower bound is not zero, return nullptr as we only support zero lower bounds
+    if (attrLb.getInt() != 0) {
+      llvm::dbgs() << innerForallOp << "expected lower bound to be zero\n";
+      return nullptr;
+    }
+  }
+
+  // check upper bound must be attr
+  auto attrUb = cast<IntegerAttr>(cast<Attribute>(*upperbound));
+  int64_t innerUbVal = 0;
+  if (!attrUb) {
+    llvm::dbgs() << innerForallOp << "expected upper bound to be an IntegerAttr\n";
+    return nullptr;
+  }
+  else {
+    // If the upper bound is not one, return nullptr as we only support one upper bounds
+    if (attrUb.getInt() <= 0) {
+      llvm::dbgs() <<  innerForallOp << "expected upper bound to be greater than zero\n";
+      return nullptr;
+    }
+    else {
+      innerUbVal = attrUb.getInt();
+    }
+  }
+
+  // get outter loops upper bound value
+  auto outerUpperBound = forallOp.getMixedUpperBound()[1];
+  auto attrOuterUb = cast<IntegerAttr>(cast<Attribute>(outerUpperBound));
+  int64_t outerUbVal = 0;
+  if (!attrOuterUb) {
+    llvm::dbgs() << forallOp << "expected outer upper bound to be an IntegerAttr\n";
+    return nullptr;
+  }
+  else {
+      outerUbVal = attrOuterUb.getInt();
+  }
+
+  int64_t newUpperBound = outerUbVal * innerUbVal;
+  auto newUpperBounds = forallOp.getMixedUpperBound();
+  newUpperBounds[1] = rewriter.getIndexAttr(newUpperBound);
+  // get the location before forallOp
+
+  // check if the innerForallOp has a single output
+  // Create a new scf::ForallOp with updated upper bounds
+
+  rewriter.setInsertionPoint(forallOp);
+  auto newForallOp = rewriter.create<scf::ForallOp>(
+      forallOp.getLoc(), forallOp.getMixedLowerBound(),
+      newUpperBounds, forallOp.getMixedStep(), forallOp.getOutputs(),
+      forallOp.getMapping(),
+      /*bodyBuilderFn=*/[](OpBuilder &, Location, ValueRange) {});
+
+  // get induction variable from the outer newForallOp
+  auto outerInductionVar0 = newForallOp.getInductionVars()[0];
+  auto outerInductionVar1 = newForallOp.getInductionVars()[1];
+
+  rewriter.setInsertionPointToStart(newForallOp.getBody());
+  auto intVal = rewriter.create<arith::ConstantIntOp>(
+      forallOp.getLoc(), innerUbVal, rewriter.getIntegerType(64));
+
+  auto divOp = rewriter.create<arith::DivUIOp>(
+      forallOp.getLoc(), outerInductionVar1, intVal);
+
+  auto moduloOp = rewriter.create<arith::RemUIOp>(
+      forallOp.getLoc(), outerInductionVar1, intVal);
+
+  auto reduction_input = innerForallOp.getResults()[0];
+
+  // get the shape of reduction_input, discard the last dimension, create a new tensor with the new shape
+  auto reductionType = cast<RankedTensorType>(reduction_input.getType());
+  auto reductionShape = reductionType.getShape();
+  SmallVector<int64_t> newReductionShape(reductionShape.begin(),
+                                         reductionShape.end() - 1);
+  auto newReductionType = RankedTensorType::get(
+      newReductionShape, reductionType.getElementType(),
+      reductionType.getEncoding());
+  // create a new tensor with the new shape
+  Value newReductionInput = rewriter.create<tensor::EmptyOp>(
+      forallOp.getLoc(), newReductionShape, newReductionType.getElementType(),
+      newReductionType.getEncoding());
+  // create linalg.fill op to fill the new tensor with constant 0
+  Value initValue = rewriter.create<arith::ConstantOp>(
+      forallOp.getLoc(), rewriter.getZeroAttr(newReductionType.getElementType()));
+  auto newLinalgFill = rewriter.create<linalg::FillOp>(forallOp.getLoc(),
+                                                       initValue,
+                                                       newReductionInput);
+
+  // insert a terminator to the new ForallOp body
+  auto yieldOp = rewriter.create<scf::YieldOp>(forallOp.getLoc(), newForallOp.getOutputs());
+
+  // Copy all operations from the old outer loop body to the new ForallOp body
+  Block *oldBody = forallOp.getBody();
+  Block *newBody = newForallOp.getBody();
+  auto innerOutType = innerForallOp.getRegionOutArgs()[0];
+
+
+  rewriter.setInsertionPoint(yieldOp);
+
+  auto oldOutInductionVar = forallOp.getInductionVars()[1];
+  auto oldInnerInductionVar = innerForallOp.getInductionVars()[0];
+  oldOutInductionVar.replaceAllUsesWith(divOp.getResult());
+  oldInnerInductionVar.replaceAllUsesWith(moduloOp.getResult());
+
+  // get the innerForallOp's body, then get the first op in the body
+  auto firstOp = *(innerForallOp.getBody()->getOps<tensor::ExtractSliceOp>().begin());
+  firstOp.getResult().replaceAllUsesWith(newLinalgFill.getResult(0));
+
+
+  for (auto &op : llvm::make_early_inc_range(*oldBody)) {
+    // Do not move the terminator (scf.yield), as the new op already has one
+    op.dump();
+    if (llvm::isa<scf::YieldOp>(op))
+      continue;
+
+    else if (llvm::isa<scf::ForallOp>(op)) {
+      scf::ForallOp innerOp = llvm::cast<scf::ForallOp>(op);
+      Block *innerBody = innerOp.getBody();
+
+      for (auto &op1 : llvm::make_early_inc_range(*innerBody)) {
+        if (&op1 == firstOp.getOperation()) {
+          // first op is not going to be copied.
+          continue;
+        }
+        else if (llvm::isa<scf::InParallelOp>(op1)) {
+          // skip the forall.in_parallel op with insert_slice
+          continue;
+        }
+        else if (llvm::isa<scf::YieldOp>(op1)) {
+          continue;
+        }
+        else {
+          rewriter.moveOpBefore(&op1, yieldOp.getOperation());
+        }
+      }
+      continue;
+    }
+    else if (llvm::isa<tensor::EmptyOp>(op)) {
+      continue;
+    }
+    else if (llvm::isa<linalg::FillOp>(op)) {
+      continue;
+    }
+    else if (llvm::isa<linalg::ReduceOp>(op)) {
+      // create a dma reduce op on it
+      auto reduceOp = llvm::cast<linalg::ReduceOp>(op);
+      auto src = reduceOp.getInputs()[0];
+      auto dst = reduceOp.getInits()[0];
+      llvm::SmallVector<OpFoldResult> offsets;
+      offsets.push_back(rewriter.getIndexAttr(0));
+      offsets.push_back(rewriter.getIndexAttr(0));
+      llvm::SmallVector<mlir::Value> dynamicOffsets;
+      llvm::SmallVector<int64_t> staticOffsets;
+      dispatchIndexOpFoldResults(offsets, dynamicOffsets,
+                                 staticOffsets);
+
+      llvm::SmallVector<OpFoldResult> strides;
+      strides.push_back(rewriter.getIndexAttr(1));
+      strides.push_back(rewriter.getIndexAttr(1));
+      llvm::SmallVector<mlir::Value> dynamicStrides;
+      llvm::SmallVector<int64_t> staticStrides;
+      dispatchIndexOpFoldResults(strides, dynamicStrides,
+                                 staticStrides);
+
+      llvm::SmallVector<OpFoldResult> sizes;
+      sizes.push_back(rewriter.getIndexAttr(reductionShape[0]));
+      sizes.push_back(rewriter.getIndexAttr(reductionShape[1]));
+      llvm::SmallVector<mlir::Value> dynamicSizes;
+      llvm::SmallVector<int64_t> staticSizes;
+      dispatchIndexOpFoldResults(sizes, dynamicSizes,
+                                 staticSizes);
+
+      auto result = rewriter.create<kestrel::DMAReduceOp>(forallOp.getLoc(),
+                                                          dst.getType(),
+                                                          src,
+                                                          dst,
+                                                          dynamicOffsets,
+                                                      dynamicStrides,
+                                                      dynamicSizes,
+                                                      staticOffsets,
+                                                      staticStrides,
+                                                      staticSizes);
+
+      reduceOp.getResult(0).replaceAllUsesWith(result->getResult(0));
+    }
+    else {
+      rewriter.moveOpBefore(&op, yieldOp.getOperation());
+    }
+  }
+  // rewriter.inlineRegionBefore(forallOp.getRegion(), newForallOp.getRegion(),
+  //                             newForallOp.getRegion().end());
+
+
+  // move all the innerForallOp's body operations to the outer forallOp
+  // and remove the innerForallOp
+  for (Operation &op : innerForallOp.getBody()->getOperations()) {
+    auto newOp = rewriter.clone(op);
+    rewriter.insert(newOp);
+  }
+  // remove the innerForallOp
+  rewriter.eraseOp(innerForallOp);
+
+  if (forallOp.getMapping().value().size() != 1) {
+    llvm::dbgs() << forallOp << "expected single mapping\n";
+    return nullptr;
+  }
+
+  // Create a new forallOp with the same bounds, outputs, and mapping as the outer forallOp
+  // but with an empty body
+  // Note: The new forallOp will have the same location as the outer forallOp
+  // and will be inserted at the same point in the IR
+  // as the outer forallOp, so it will replace the outer forallOp in the IR
+
+
+
   rewriter.replaceOp(forallOp, newForallOp);
   return newForallOp;
 }
@@ -164,8 +387,23 @@ scf::ForallOp mergeInnerScfForAll(
       diag.attachNote(targetOp->getLoc()) << "target op";
       return diag;
     }
+    scf::ForallOp innerForallOp = nullptr;
+    forallOp.getBody()->walk([&](scf::ForallOp op) {
+      if (op != forallOp && !innerForallOp) {
+        innerForallOp = op;
+        return WalkResult::interrupt();
+      }
+      return WalkResult::advance();
+    });
 
-    scf::ForallOp merged = mergeInnerScfForAll(rewriter, forallOp);
+    if (!innerForallOp) {
+      DiagnosedSilenceableFailure diag = emitSilenceableError()
+                                         << "no inner scf.forall op found";
+      diag.attachNote(location) << "target op";
+      return diag;
+    }
+
+    scf::ForallOp merged = mergeInnerScfForAll(rewriter, forallOp, innerForallOp);
     if (!merged) {
       DiagnosedSilenceableFailure diag = emitSilenceableError()
                                          << "failed to merge inner scf.forall";
